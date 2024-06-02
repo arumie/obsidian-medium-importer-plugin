9d70894ee1d011ac8d87966e9be5ce2c275ec2e7
arumie <david.zachariae@gmail.com>
2024-06-02 20:54:37 +0200

    0.1.1

--- release.sh
+++
@@ -1,3 +0,0 @@
-PACKAGE_VERSION=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-echo "Releasing version $PACKAGE_VERSION"
-$(git tag $PACKAGE_VERSION)
